e0573244462ad5eab99e82e01f36b56789c6fc7a
Abdilmajid Ibrahim <abdilmajidibrahim@gmail.com>
2018-12-16 21:34:18 -0500

    database recieved form data

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const pg = require('pg');
 
 
+
 //Connect Express Server to Postgress
 const config = {
   user: 'postgres',
@@ -32,15 +33,78 @@ router.get('/products', (req, res) => {
 
 router.post('/orders', (req, res) => {
   const {name, email, order_items} = req.body.orders;
-  pool.connect()
-    .then(() => {
-      const sql = 'INSERT INTO orders(name, email) VALUES ($1, $2), INSERT INTO order_items(product_id, qty) VALUES ($3, $4);'
-      order_items.forEach(data => {
-        const params = [name, email, data.product_id, data.qty]
-        return pool.query(sql, params)
+  const sql1 = 'INSERT INTO orders(name, email) VALUES ($1, $2) RETURNING id'
+  const sql2 = 'INSERT INTO order_items(order_id, product_id, qty) VALUES ($1, $2, $3)'
+
+  pool.connect((err, pool, done) => {
+    const shouldAbort = (err) => {
+      if(err) {
+        console.error('Error in transaction', err.stack)
+        pool.query('ROOLBACK', (err) => {
+          if(err) {
+            console.error('Error rolling back client', err.stack)
+          }
+          done()
+        })
+      }
+      return !!err
+    }
+    pool.query('BEGIN', (err) => {
+      if(shouldAbort(err)) return
+      pool.query(sql1, [name, email], (err, res) => {
+        if(shouldAbort(err)) return
+
+        const id = res.rows[0].id
+        order_items.forEach(data => {
+          return pool.query(sql2, [id, data.product_id, data.qty], (err, res) => {
+            if(shouldAbort(err)) return
+            pool.query('COMMIT', (err) => {
+              if(err) {
+                console.error('Error committing transaction', err.stack)
+              }
+              done()
+            })
+          })
+        })
       })
     })
+  })
 })
+
+
+
+// router.post('/orders', (req, res) => {
+//   const {name, email, order_items} = req.body.orders;
+//   const {id} = req.params.id;
+//   console.log(id) 
+//   pool.connect()
+//     .then((err, client) => {
+//       if(err) return cb(err);
+//       const sql1 = 'INSERT INTO orders(name, email) VALUES ($1, $2) RETURNING id' 
+//       const sql2 = 'INSERT INTO order_items(order_id, product_id, qty) VALUES ($1, $2, $3)'
+//       pool.query(sql1, params1, (err, results) => {
+//         if(err) {
+//           return cb(err)
+//         }
+//         const params1 = [name, email]
+//       })
+//       order_items.forEach(data => {
+//         const params = [name, email]
+//         return pool.query(sql, params)
+//       })
+//     })
+//     .then(() => {
+//       const sql = 'INSERT INTO order_items(order_id, product_id, qty) VALUES ($1, $2, $3);'
+//       order_items.forEach(data => {
+//         const params = [id, data.product_id, data.qty]
+//         return pool.query(sql, params)
+//       })
+//     })
+//     .then(data => {
+//       console.log(data)
+//     })
+// })
+
 
 
 
